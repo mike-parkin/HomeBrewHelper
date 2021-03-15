76580c59eb5006ea232c564e963c390ea1f55045
mike-parkin <michael.parkin1988@gmail.com>
2021-03-16 11:43:42 +1300

    removed commented out code from file

--- client/apis/hops.js
+++ client/apis/hops.js
@@ -1,9 +1,6 @@
 import request from 'superagent'
-// import { apiKey } from '../../apiKey'
 
 const baseUrl = '/api/v1/hops'
-
-// const baseUrl = 'https://sandbox-api.brewerydb.com/v2/'
 
 export function getHops() {
     return request
