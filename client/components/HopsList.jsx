add8c4bd6f8ce01118f294530b609cfcc6afb428
mike-parkin <michael.parkin1988@gmail.com>
2021-03-16 10:44:33 +1300

    cors issues arising, need to re-route

--- client/components/HopsList.jsx
+++ client/components/HopsList.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+
+import { getHops } from '../apis/brewerydb'
 
 const HopsList = (props) => {
+    const [ hops, setHops ] = useState({})
 
+    const fetchHops = () => {
+        getHops()
+            .then(hops => {
+                setHops(hops)
+            })
+    }
+
+    useEffect(() => {
+        fetchHops()
+    }, [])
+    
     return (
-        <h1>List of hops</h1>
+        <>
+            <h1>List of hops</h1>
+            <ul>
+                {/* {hops.data.map(hop => {
+                    <li>{hop.name}</li>
+                })} */}
+            </ul>
+        </>
     )
 }
 
